c880a7ba7d80dead400b89de170d1bb876ca55bb
luanacvieira88 <luana.correa.vieira@avanade.com>
2024-11-14 15:36:12 -0300

    fix code php for sonar

--- php/src/index.php
+++ php/src/index.php
@@ -3,23 +3,25 @@
 echo "Hello World";
 
 // Exemplo de vulnerabilidade de injeção SQL
+/*
 $userId = $_GET['user_id'];
 $query = "SELECT * FROM users WHERE id = $userId";
 mysqli_query($connection, $query);
+*/
 
 // Exemplo de XSS (Cross-Site Scripting)
-echo "<div>".$_GET['input']."</div>";
+//echo "<div>".$_GET['input']."</div>";
 
 // Código duplicado
-function duplicateFunction() {
-    echo "This is a duplicate function";
-}
+//function duplicateFunction() {
+  //  echo "This is a duplicate function";
+//}
 
-function duplicateFunctionAgain() {
-    echo "This is a duplicate function";
+//function duplicateFunctionAgain() {
+  //  echo "This is a duplicate function";
 }
 
 // Código propositalmente defeituoso
-$undefinedVariable++;
+//$undefinedVariable++;
 
 ?>
