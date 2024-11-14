1a3ee89f4522fa4c18acd1d47939bf785a70cfb2
luanacvieira88 <luana.correa.vieira@avanade.com>
2024-11-14 11:08:06 -0300

    Update index.php

--- php/src/index.php
+++ php/src/index.php
@@ -1,3 +1,16 @@
 <?php
-echo "Hello, World!";
+// Código com duplicação e má qualidade
+echo "Hello World";
+
+// Código duplicado
+function duplicateFunction() {
+    echo "This is a duplicate function";
+}
+
+function duplicateFunctionAgain() {
+    echo "This is a duplicate function";
+}
+
+// Código propositalmente defeituoso
+$undefinedVariable++;
 ?>
